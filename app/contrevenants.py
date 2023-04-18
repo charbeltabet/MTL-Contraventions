7cf7c5abcd195c23aa7a528690b523d19b7b045d
Charbel Tabet <charbeltabet@live.com>
2023-04-17 22:24:25 -0400

    Fix url names

--- app/contrevenants.py
+++ app/contrevenants.py
@@ -1,26 +1,32 @@
 from flask import Blueprint, render_template, request, jsonify
 from app.models.violation import Violation
 
-bp = Blueprint('contrevenants', __name__, url_prefix='/contrevenants')
+bp = Blueprint('contrevenants', __name__, url_prefix='/')
 
 @bp.route('/')
+def index():
+    return render_template('contrevenants/index.html', establishments=Violation.all_establishments())
+
+@bp.route('/contrevenants')
 def get_contrevenants():
-    if (request.args.get('establishment') or
-        (request.args.get('owner')) or
-        (request.args.get('address'))):
-        violations = Violation.search(
-            establishment=request.args.get('establishment'),
-            owner=request.args.get('owner'),
-            address=request.args.get('address'),
-        )
-        if (request.content_type == 'application/json'):
-            return [violation.to_dict() for violation in violations]
-        return render_template('contrevenants/search_results.html', violations=violations)
-    elif (request.args.get('du') and
-          (request.args.get('au'))):
-        return Violation.search_between_dates(
-            start_date=request.args.get('du'),
-            end_date=request.args.get('au'),
-        )
-    else:
-        return render_template('contrevenants/index.html', establishments=Violation.all_establishments())
+    if not request.args.get('du') or not request.args.get('au'):
+        return "Bad request", 400
+
+    return Violation.search_between_dates(
+        start_date=request.args.get('du'),
+        end_date=request.args.get('au'),
+    )
+
+@bp.route('/contrevenants/search')
+def search_contrevenants():
+    if not request.args.get('establishment') and not request.args.get('owner') and not request.args.get('address'):
+        return "Bad request", 400
+
+    violations = Violation.search(
+        establishment=request.args.get('establishment'),
+        owner=request.args.get('owner'),
+        address=request.args.get('address'),
+    )
+    if (request.content_type == 'application/json'):
+        return [violation.to_dict() for violation in violations]
+    return render_template('contrevenants/search_results.html', violations=violations)
