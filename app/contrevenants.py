00a67eb75ace2449a16ac68afea8feaf6c215bc8
Charbel Tabet <charbeltabet@live.com>
2023-04-18 02:14:15 -0400

    Add csv and xml support

--- app/contrevenants.py
+++ app/contrevenants.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, Response, jsonify
 from app.models.violation import Violation
+from dicttoxml import dicttoxml
+import csv, io
 
 bp = Blueprint('contrevenants', __name__, url_prefix='/')
 
@@ -14,10 +16,10 @@ def index():
 @bp.route('/contrevenants')
 def get_contrevenants():
     if request.args.get('du') and request.args.get('au'):
-        return Violation.count_by_establishment_between_dates(
+        return violations_response(Violation.count_by_establishment_between_dates(
             start_date=request.args.get('du'),
             end_date=request.args.get('au'),
-        )
+        ), request.content_type)
     elif not request.args.get('du') and not request.args.get('au'):
         return Violation.count_by_establishment()
     return "Bad request", 400
@@ -35,3 +37,17 @@ def search_contrevenants():
     if (request.content_type == 'application/json'):
         return [violation.to_dict() for violation in violations]
     return render_template('contrevenants/search_results.html', violations=violations)
+
+def violations_response(violations, content_type):
+    if content_type == 'application/json':
+        return jsonify([violation.to_dict() for violation in violations])
+    elif content_type == 'application/xml':
+        xml = dicttoxml([violation.to_dict() for violation in violations])
+        return Response(xml, content_type='application/xml')
+    elif content_type == 'text/csv':
+        output = io.StringIO()
+        writer = csv.writer(output)
+        writer.writerow(['Establishment', 'Violation Count'])
+        for violation in violations:
+            writer.writerow([violation.establishment, violation.violation_count])
+        return Response(output.getvalue(), content_type='text/csv')
